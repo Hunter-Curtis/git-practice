4067fd649f6450e9cf6af5daca4f249d441d74a6
Admin <admin@Admins-MacBook-Pro.local>
2019-04-30 20:15:29 -0600

    number changed

--- hunter.js
+++ hunter.js
@@ -2,5 +2,5 @@ console.log('hi my name is hunter')
 console.log('hello This is javascript!')
 console.log('no, i think in like pizza better!')
 
-const stuff = 7 + 7;
-console.log(stuff);
+const number = 7 + 7;
+console.log(number);
